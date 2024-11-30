97347b90004c69215629099c95036a2dbc1aedb4
ivan.lukianenko <ivan.lukianenko@dsr-corporation.com>
2024-11-30 17:52:44 +0500

    Get balance is implemented

--- bot.py
+++ bot.py
@@ -1,40 +1,87 @@
-from pybit.unified_trading import HTTP
-from dotenv import load_dotenv
 import os
 
-# Загружаем переменные окружения из файла .env
+from pybit import exceptions
+from pybit.unified_trading import HTTP
+
+import pandas as pd
+from pandas import DataFrame
+from dotenv import load_dotenv
+
+pd.set_option('display.max_rows', 500)
+pd.set_option('display.max_columns', 500)
+pd.set_option('display.width', 1000)
+
 load_dotenv()
+API_KEY = os.getenv("BB_API_KEY")
+SECRET_KEY = os.getenv("BB_SECRET_KEY")
 
-# Получаем API ключи из переменных окружения
-api_key = os.getenv("API_KEY")
-api_secret = os.getenv("API_SECRET")
+def log_limits(headers : dict):
+    print(f"Limits  {headers.get('X-Bapi-Limit-Status')} / {headers.get('X-Bapi-Limit')}")
 
-# Инициализируем клиент Bybit
-# Для тестового API используйте: "https://api-testnet.bybit.com"
-# Для реального API используйте: "https://api.bybit.com"
-session = HTTP(
-    testnet=False,  # Измените на False для реального API
-    api_key=api_key,
-    api_secret=api_secret,
-)
+def assets(cl : HTTP):
+    """
+    Получаю остатки на UNIFIED аккаунте (не на кошельке целиком)
+    копитрейдинг, фандинг, инверс - здесь не видно
+    :param cl:
+    :return:
+    """
+    r, _, h = cl.get_wallet_balance(accountType="UNIFIED")
+    r = r.get('result', {}).get('list', [])[0]
 
+    total_balance = float(r.get('totalWalletBalance', '0.0'))
+    coins = [f"{float(c.get('walletBalance', '0.0')):>12.6f} {c.get('coin'):>12}" for c in r.get('coin', [])]
 
-# Пример получения баланса аккаунта
-def get_wallet_balance():
-    return session.get_wallet_balance(accountType="UNIFIED")
+    print("\n".join(coins))
+    print(f"---\nTotal: {total_balance:>18.2f}\n")
 
+    log_limits(h)
 
-# Пример получения текущей цены
-def get_current_price(symbol="BTCUSDT"):
-    return session.get_tickers(category="spot", symbol=symbol)
+def get_transfers(cl : HTTP):
+    """
+    Логи переводов средств на аккаунте
 
+    ! в этом примере БЕЗ пагинации !
+    подписывайся https://www.youtube.com/@AzzraelCode
+    чтобы не пропустить про пагинацию
 
-# Получаем и выводим баланс кошелька
-balance = get_wallet_balance()
-print("Баланс кошелька:")
-print(balance)
+    :param cl:
+    :return:
+    """
+    r, _, h = cl.get_transaction_log()
 
-# Получаем и выводим текущую цену
-price = get_current_price()
-print("Текущая цена:")
-print(price)
+    df = DataFrame([dict(
+        currency=e.get('currency'),
+        type=e.get('type'),
+        change=e.get('change'),
+        cashBalance=e.get('cashBalance'),
+        transactionTime=int(e.get('transactionTime')),
+    ) for e in r.get('result', {}).get('list', []) if e.get('type').startswith('TRANSFER')])
+
+    df['transactionTime']=pd.to_datetime(df['transactionTime'], unit='ms')
+    df.sort_values(by=['transactionTime'], inplace=True, ascending=False)
+    print(df)
+
+    log_limits(h)
+
+def main():
+
+    cl = HTTP(
+        api_key=API_KEY,
+        api_secret=SECRET_KEY,
+        recv_window=60000,
+        return_response_headers=True,
+    )
+
+    try:
+        assets(cl)
+        get_transfers(cl)
+
+    except exceptions.InvalidRequestError as e:
+        print("ByBit Request Error", e.status_code, e.message, sep=" | ")
+    except exceptions.FailedRequestError as e:
+        print("ByBit Request Failed", e.status_code, e.message, sep=" | ")
+    except Exception as e:
+        print(e)
+
+if __name__ == '__main__':
+    main()
